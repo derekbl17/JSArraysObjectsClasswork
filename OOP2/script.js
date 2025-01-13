6faaee0ff0d03b886986918af5334e7a15b0a5ed
derekbl17 <derekc1fill@gmail.com>
2025-01-13 14:03:22 +0200

    works with local storage

--- OOP2/script.js
+++ OOP2/script.js
@@ -26,29 +26,35 @@ class Birds extends Animal {
   }
 }
 
-const catBtn = document.getElementById("cat");
-const catSubBtn = document.getElementById("submitCat");
-const fishBtn = document.getElementById("fish");
-const fishSubBtn = document.getElementById("submitFish");
-const birdBtn = document.getElementById("bird");
-const birdSubBtn = document.getElementById("submitBird");
-const addedArea = document.getElementById("added");
-const addedArray = [];
-let cnt = 0;
+const catBtn = document.getElementById("cat"); // cat form select
+const catSubBtn = document.getElementById("submitCat"); // cat form submit
+const fishBtn = document.getElementById("fish"); // fish form select
+const fishSubBtn = document.getElementById("submitFish"); // fish form submit
+const birdBtn = document.getElementById("bird"); // bird form select
+const birdSubBtn = document.getElementById("submitBird"); // bird form submit
+const addedArea = document.getElementById("added"); // section under form for submitted pets
+const addedArray = JSON.parse(localStorage.getItem("pets")) || []; // array for created objects(submitted pets)
+let cnt = 0; // array index counter
+while (addedArray.length > cnt) {
+  displayAnimal(1);
+}
 
+// creates cat object
 function addCat() {
   const cName = document.getElementById("catName").value;
   const cAge = document.getElementById("catAge").value;
   const furC = document.getElementById("furColor").value;
   const furT = document.getElementById("furType").value;
+  // check if no empty inputs
   if ((cName, cAge, furC, furT)) {
     addedArray[cnt] = new Cats(cName, cAge, furC, furT);
-    displayAnimal();
-    cnt++;
+    displayAnimal(1);
+    // resets input values after submission
     document.getElementById("catName").value = "";
     document.getElementById("catAge").value = "";
     document.getElementById("furColor").value = "";
     document.getElementById("furType").value = "";
+    // if empty input, alerts
   } else alert("invalid input");
 }
 
@@ -59,8 +65,7 @@ function addFish() {
   const fWeight = document.getElementById("weight").value;
   if ((fName, fAge, fScaleColor, fWeight)) {
     addedArray[cnt] = new Fish(fName, fAge, fScaleColor, fWeight);
-    displayAnimal();
-    cnt++;
+    displayAnimal(1);
     document.getElementById("nameF").value = "";
     document.getElementById("ageF").value = "";
     document.getElementById("scaleColor").value = "";
@@ -74,39 +79,41 @@ function addBird() {
   const bBeakType = document.getElementById("beakType").value;
   if ((bName, bAge, bFeatherColor, bBeakType)) {
     addedArray[cnt] = new Birds(bName, bAge, bFeatherColor, bBeakType);
-    displayAnimal();
-    cnt++;
+    displayAnimal(1);
     document.getElementById("nameB").value = "";
     document.getElementById("ageB").value = "";
     document.getElementById("featherColor").value = "";
     document.getElementById("beakType").value = "";
   } else alert("invalid input");
 }
+// on page load select cat form
 window.addEventListener("load", (e) => {
   catBtn.click();
 });
 
 catBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("Zzz");
+  document.getElementById("catField").style = "display:block;";
   document.getElementById("fishField").style = "display:none;";
   document.getElementById("birdField").style = "display:none;";
-  document.getElementById("catField").style = "display:block;";
+  catBtn.style = "background:#684db9;";
+  fishBtn.style = "";
+  birdBtn.style = "";
 });
 
 catSubBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("z");
   addCat();
-  console.log(addedArray);
 });
 
 fishBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("Zzz");
+  document.getElementById("catField").style = "display:none;";
   document.getElementById("fishField").style = "display:block;";
   document.getElementById("birdField").style = "display:none;";
-  document.getElementById("catField").style = "display:none;";
+  catBtn.style = "";
+  fishBtn.style = "background:#684db9;";
+  birdBtn.style = "";
 });
 
 fishSubBtn.addEventListener("click", (e) => {
@@ -116,10 +123,12 @@ fishSubBtn.addEventListener("click", (e) => {
 
 birdBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("Zzz");
+  document.getElementById("catField").style = "display:none;";
   document.getElementById("fishField").style = "display:none;";
   document.getElementById("birdField").style = "display:block;";
-  document.getElementById("catField").style = "display:none;";
+  catBtn.style = "";
+  fishBtn.style = "";
+  birdBtn.style = "background:#684db9;";
 });
 
 birdSubBtn.addEventListener("click", (e) => {
@@ -127,13 +136,28 @@ birdSubBtn.addEventListener("click", (e) => {
   addBird();
 });
 
-function displayAnimal() {
+// add to section last submitted animal
+function displayAnimal(param = 0) {
   const form = document.createElement("form");
   form.id = "addedForm";
+  const remButton = document.createElement("button");
+  const targetIndex = cnt;
+  remButton.innerText = "remove";
+  remButton.addEventListener("click", (e) => {
+    cnt--;
+    e.preventDefault();
+    addedArray.splice(targetIndex, 1);
+    console.log(addedArray);
+    displayAnimal();
+    location.reload();
+  });
   for (let k in addedArray[cnt]) {
     const pEl = document.createElement("p");
     pEl.innerText = `${k}: ${addedArray[cnt][k]}`;
     form.append(pEl);
   }
-  addedArea.append(form);
+  form.append(remButton);
+  param == 1 ? addedArea.append(form) : 0;
+  localStorage.setItem("pets", JSON.stringify(addedArray));
+  cnt++;
 }
